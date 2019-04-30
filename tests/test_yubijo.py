c4ad6d31b9c87bda44bf98ed520b94447b070edd
nagisc007 <nagisc007@yahoo.co.jp>
2019-04-30 10:50:45 +0900

    Added: yubijo test files

---
+++ tests/test_yubijo.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Test: yubijo
+"""
+import unittest
+from storybuilder.builder import testutils as utl
+from src.yubijo.story import world, story
+
+
+_FILENAME = "yubijo.story.py"
+
+
+class StoryTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        utl.print_test_title(_FILENAME, "yubijo project")
+
+    def setUp(self):
+        self.w = world()
+        self.story = story(self.w)
+
+    def test_is_all_actions(self):
+        self.assertTrue(utl.is_all_actions_in(self.story))
+
+    @unittest.skip('use each current')
+    def test_exists_looking(self):
+        self.assertTrue(utl.exists_looking_from_master(self.story, self.ma))
+
+    def test_followed_flags(self):
+        self.assertTrue(utl.followed_all_flags(self.story))
+
+    @unittest.skip('wip')
+    def test_has_basic_infos(self):
+        utl.exists_basic_infos_by_data(self,
+                [
+                ("story", self.story, self.w.zenzo, self.w.akebi),
+                ])
+
+    @unittest.skip('wip')
+    def test_has_outline_infos(self):
+        w = self.w
+        utl.exists_outline_infos_by_data(self,
+                [
+                ("story", self.story,
+                    w.zenzo.be(),
+                    w.zenzo.be(),
+                    w.zenzo.be(),
+                    w.zenzo.be(),
+                    True),
+                ])
+
