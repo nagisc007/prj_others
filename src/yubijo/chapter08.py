e73536cbd048ad4a308e22740e79bccf088d79cf
nagisc007 <nagisc007@yahoo.co.jp>
2019-05-01 09:55:28 +0900

    Added: chapter files

---
+++ src/yubijo/chapter08.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Story: yubijo chapter08.
+"""
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append('storybuilder')
+
+from storybuilder.builder import world as wd
+from . import config as cnf
+
+
+# episodes
+
+
+# main
+def story(w: wd.World):
+    return (w.maintitle("第八話"),
+            w.zenzo.be(w.stage.chineseshop, w.day.meet),
+            w.zenzo.meet(w.akebi),
+            )
+
+
+def main(): # pragma: no cover
+    from . import story as mainstory
+    w = mainstory.world()
+    return w.build(story(w))
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
+
