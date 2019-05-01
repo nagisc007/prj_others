f84e14e26fb8de301c7b8ef83e5d7cba871baa51
nagisc007 <nagisc007@yahoo.co.jp>
2019-05-01 16:37:59 +0900

    Fixed: imports

--- src/yubijo/chapter03.py
+++ src/yubijo/chapter03.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 sys.path.append('storybuilder')
 
 from storybuilder.builder import world as wd
-from . import config as cnf
+from src.yubijo import config as cnf
 
 
 # episodes
