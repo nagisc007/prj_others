3ebb4a7a04c54df89d14c8d0bfa993999932eb3c
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-05-02 11:21:17 +0900

    Added: chapter 6 (#18)
    checked list

--- src/yubijo/chapter06.py
+++ src/yubijo/chapter06.py
@@ -15,9 +15,25 @@ from src.yubijo import config as cnf
 
 # main
 def story(w: wd.World):
-    return (w.maintitle("第六話"),
-            w.zenzo.be(w.stage.chineseshop, w.day.meet),
-            w.zenzo.meet(w.akebi),
+    return (w.maintitle("第六話　ハーレムが地獄でも問題ないよね？"),
+            w.zenzo.be(w.stage.office, w.day.meet3),
+            w.zenzo.remember(w.minako),
+            w.zenzo.do("失恋"),
+            w.zenzo.look("new love", "$want"),
+            w.murako.ask(w.zenzo, "仕事したら？"),
+            w.zenzo.ask(w.murako, w.i.ability),
+            w.murako.reply("よく知らない"),
+            w.murako.talk(w.zenzo, w.stage.ghosthospital),
+            w.zenzo.ask(w.stage.ghosthospital),
+            w.murako.talk(w.i.ghospital_info),
+            w.zenzo.go(w.stage.ghosthospital),
+            w.zenzo.meet(w.beniko),
+            w.zenzo.meet(w.mia),
+            w.zenzo.meet(w.kii),
+            w.zenzo.hear(w.i.beniko_reason),
+            w.zenzo.hear(w.i.mia_reason),
+            w.zenzo.hear(w.i.kii_reason),
+            w.zenzo.do("selection", "三人から"),
             )
 
 
--- src/yubijo/config.py
+++ src/yubijo/config.py
@@ -12,8 +12,12 @@ CHARAS = (
         ("akebi", "山野井山女", 17, "female", "幽霊", "me:わたし", "自殺した女子高生"),
         ("murako", "田村邑子", 33, "female", "退魔師", "me:私"),
         ("cap", "柊聖文", 56, "male", "所長", "me:僕:zenzo:善ちゃん:murako:ムラムラちゃん"),
-        # chapter4
+        # chapter 4
         ("minako", "水辺水菜子", 28, "female", "幽霊", "me:ワタシ"),
+        # chapter 6
+        ("beniko", "紅沢紅子", 35, "female", "幽霊", "me:私"),
+        ("mia", "青井未青", 22, "female", "幽霊", "me:みぃ"),
+        ("kii", "黄川田黄衣", 17, "female", "幽霊", "me:アタイ"),
         )
 
 
@@ -22,15 +26,24 @@ STAGES = (
         ("apart", "畠中のアパート"),
         ("office", "退魔師事務所"),
         ("chineseshop", "中華飯店"),
+        # chapter 1,2
         ("ghosthome", "幽霊屋敷"),
+        # chapter 4,5
         ("ghostschool", "幽霊廃校"),
+        # chapter 6,7
         ("ghosthospital", "幽霊病院"),
+        # chapter 8
+        ("ghosttonnel", "幽霊トンネル"),
+        ("ghostworld", "幽霊世界"),
         )
 
 
 DAYS = (
         ("meet", "出会いの日"),
+        # chapter 4
         ("meet2", "出会いの日２"),
+        # chapter 6
+        ("meet3", "出会いの日３"),
         )
 
 
@@ -65,6 +78,11 @@ INFOS = (
         ("minako_reason", "水菜子の事情"),
         ("minako_death", "水菜子の死因"),
         ("minako_dream", "水菜子の夢"),
+        # chapter 6
+        ("ghospital_info", "幽霊病院の噂"),
+        ("beniko_reason", "紅子の事情"),
+        ("mia_reason", "未青の事情"),
+        ("kii_reason", "黄衣の事情"),
         )
 
 
