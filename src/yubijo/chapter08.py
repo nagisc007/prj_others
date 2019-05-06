2d83c549d6c9f5b9e76055e727a2ae15317eb59c
nagisc007 <nagisc007@yahoo.co.jp>
2019-05-06 10:16:15 +0900

    Added: chapter 8 episodes

--- src/yubijo/chapter08.py
+++ src/yubijo/chapter08.py
@@ -13,28 +13,43 @@ from src.yubijo import config as cnf
 # episodes
 def ep_avant(w: wd.World):
     return [w.chaptertitle("彼女の姉"),
-            w.zenzo.go(w.stage.office, w.day.meet4, w.murako),
+            w.zenzo.go(w.stage.apart, w.day.meet4, w.machiko),
             w.zenzo.meet(w.machiko),
             w.zenzo.know(w.machiko, w.murako, "sister"),
             w.machiko.talk(w.zenzo, w.flag.secret_murako),
             w.zenzo.hear(w.machiko, w.flag.secret_murako),
+            w.zenzo.know(w.machiko, "$want"),
+            w.zenzo.do(w.machiko, "救出"),
+            w.zenzo.go(w.machiko, "ついていく"),
+            w.zenzo.go(w.stage.ghosttonnel),
+            w.zenzo.go(w.stage.murakohome),
+            w.zenzo.be("監禁"),
             ]
 
 def ep_bounded(w: wd.World):
     return [w.chaptertitle("囚われの善蔵"),
+            w.zenzo.be("監禁", w.stage.murakohome, w.day.meet4),
             w.zenzo.know(w.flag.secret_murako, "$must"),
             w.zenzo.deal("search", w.murako),
             w.zenzo.go(w.stage.ghosttonnel),
             w.zenzo.go(w.stage.murakohome),
             w.zenzo.look("research", w.stage.murakohome),
+            w.zenzo.go("逃げ出す"),
+            w.zenzo.ask(w.machiko, w.i.machiko_reason),
+            w.zenzo.know(w.i.firstlove, "関係している"),
             ]
 
 def ep_secretfirstlove(w: wd.World):
     return [w.chaptertitle("初恋の秘密"),
+            w.zenzo.be(w.stage.murakohome, w.day.meet4),
             w.zenzo.know(w.deflag.secret_murako),
             w.zenzo.deal(w.machiko, "監禁"),
             # TODO: secret world
             w.zenzo.know(w.deflag.secret_world),
+            w.zenzo.know(w.murako, "$must"),
+            w.zenzo.remember(w.i.firstlove, w.murako),
+            w.zenzo.ask(w.machiko),
+            w.zenzo.know(w.i.murako_dead),
             ]
 
 
