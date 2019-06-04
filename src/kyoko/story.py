d3e85b5b53877a172cf4988fc8a3800f6b3b2584
nagisc007 <nagisc007@yahoo.co.jp>
2019-06-04 09:21:52 +0900

    Added: kyoko-san project files

---
+++ src/kyoko/story.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Story: the kyoko-san
+"""
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append('storybuilder')
+
+from storybuilder.builder import world as wd
+from src.kyoko import config as cnf
+
+
+# main
+def world():
+    w = wd.World("The kyoko-san project")
+    w.set_db(cnf.CHARAS,
+            cnf.STAGES,
+            cnf.DAYS,
+            cnf.ITEMS,
+            cnf.INFOS,
+            cnf.FLAGS,
+            )
+    return w
+
+def story(w: wd.World):
+    return (w.maintitle("今日子さんの夕沈み"),
+            w.kyoko.be(w.stage.apart, w.day.current, w.hiiragi),
+            )
+
+def main(): # pragma: no cover
+    w = world()
+    return w.build(story(w))
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
+
