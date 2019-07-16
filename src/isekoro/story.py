1c601ec61dd5eb574a535bed94d805b3f7034d5a
nagisc007 <nagisc007@yahoo.co.jp>
2019-07-16 10:50:27 +0900

    Added: episode outline

--- src/isekoro/story.py
+++ src/isekoro/story.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Story: re_isekai_koroshi
+"""Story: re_isekai_sunami
 """
 import os
 import sys
@@ -8,26 +8,71 @@ sys.path.append('storybuilder')
 
 from storybuilder.builder import world as wd
 from src.noplot import config as cnf
+THM = cnf.THEMES
+
+
+# scenes
+def sc_vanish(w: wd.World):
+    return w.scene("また異世界が消えた",
+            )
+
+def sc_gotomyworld(w: wd.World):
+    return w.scene("私の異世界に",
+            )
+
+def sc_facetoface(w: wd.World):
+    return w.scene("対峙",
+            )
+
+def sc_confession(w: wd.World):
+    return w.scene("告白",
+            )
+
+def sc_awake(w: wd.World):
+    return w.scene("そして目覚めた",
+            )
+
+# episodes
+def ep_intro(w: wd.World):
+    return (w.chaptertitle("異世界を殺すモノ"),
+            sc_vanish(w),
+            )
+
+def ep_lastisekai(w: wd.World):
+    return (w.chaptertitle("最後の異世界に"),
+            sc_gotomyworld(w),
+            sc_facetoface(w),
+                w.hiiragi.think(w.i.stop_isekoro),
+                w.hiiragi.know(w.i.isekoro),
+                w.hiiragi.go(w.stage.cherryhill),
+            )
+
+def ep_isekaikoroshi(w: wd.World):
+    return (w.chaptertitle("異世界殺し"),
+            sc_confession(w),
+            sc_awake(w),
+                w.hiiragi.know(w.i.truth),
+            )
 
 # outline
 def story_baseinfo(w: wd.World):
     return [
-            ("story", story(w), w.cameraman, w.koroshi),
+            ("story", story(w), w.hiiragi, w.sunami),
             ]
 
 def story_outline(w: wd.World):
     return [
             ("story", story(w),
-                w.cameraman.be(),
-                w.cameraman.be(),
-                w.cameraman.be(),
-                w.cameraman.be(),
+                w.hiiragi.think(w.i.stop_isekoro),
+                w.hiiragi.know(w.i.isekoro),
+                w.hiiragi.go(w.stage.cherryhill),
+                w.hiiragi.know(w.i.truth),
                 True),
             ]
 
 # main
 def world():
-    w = wd.World("Re_isekai_koroshi")
+    w = wd.World("Re_isekai_sunami")
     w.set_db(cnf.CHARAS,
             cnf.STAGES,
             cnf.DAYS,
@@ -39,7 +84,7 @@ def world():
 
 def story(w: wd.World):
     return (w.maintitle("Re:異世界殺し"),
-            w.cameraman.be(w.koroshi, w.stage.cherryhill, w.day.current),
+            w.hiiragi.be(w.sunami, w.stage.cherryhill, w.day.current),
             )
 
 def main(): # pragma: no cover
