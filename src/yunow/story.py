b26b48691db66e782149f4ef712aa6e8d3108e8f
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-07-19 09:07:07 +0900

    Added: yusha now project (#61)

---
+++ src/yunow/story.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""Story: yusha now
+"""
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append('storybuilder')
+
+from storybuilder.builder import world as wd
+from src.yunow import config as cnf
+THM = cnf.THEMES
+
+
+# scenes
+# episodes
+
+# outline
+def story_baseinfo(w: wd.World):
+    return [
+            ]
+
+def story_outline(w: wd.World):
+    return [
+            ]
+
+# main
+def world():
+    w = wd.World("Yusha now")
+    w.set_db(cnf.CHARAS,
+            cnf.STAGES,
+            cnf.DAYS,
+            cnf.ITEMS,
+            cnf.INFOS,
+            cnf.FLAGS,
+            )
+    return w
+
+def story(w: wd.World):
+    return (w.maintitle("勇者なう！"),
+            )
+
+def main(): # pragma: no cover
+    w = world()
+    return w.build(story(w))
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
+
