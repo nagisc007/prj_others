ab2bd472871faf894d7a50ec53b71af814d2ca78
nagisc007 <nagisc007@yahoo.co.jp>
2019-07-20 11:37:15 +0900

    [WIP] Added: central town

--- src/yunow/chapter1.py
+++ src/yunow/chapter1.py
@@ -34,11 +34,50 @@ def sc_Iamyusha(w: wd.World):
             yusha.talk("いやあ",
                 "しかし$meが勇者かあ"),
             yusha.look("一見すると彼はごく普通の十六歳の鼻水垂らした少年に見えるのだが",
-                "この度$kingより直々に勇者の称号を冠され",
+                "この度$n_kingより直々に勇者の称号を冠され",
                 "魔王退治を依頼された英雄の卵であった"),
             yusha.look("彼が手にしている$phoneと呼ばれる漆黒の石版は",
                 "同じ石版を持つ者に対して呟いた文字を流すことができるという特性がある"),
+            yusha.talk("お？", "$meって早速話題になってね？"),
+            yusha.look("次々と流れてくる”勇者”という文字の洪水に",
+                "彼の表情筋は緩み",
+                "空の雲行きが怪しくなっていることにも気づかないでいる"),
+            )
+
+def sc_strangetown(w: wd.World):
+    yusha = w.yusha
+    return w.scene("町の様子がおかしいぜ",
             yusha.be(w.stage.hometown),
+            yusha.ask("ん？"),
+            yusha.look("彼が異変を感じ取ったのは",
+                "$st_castle1を出てその城下町の中央広場までやってきた時だった"),
+            yusha.talk("あれ？", "さっきまでここって露店いっぱい並んでたはずじゃ"),
+            yusha.look("右肩から提げたズタ袋をまさぐり王様から貰った支度金の硬貨の感触を楽しもうとした彼は",
+                "立ち止まってぐるりと周囲を見回す"),
+            yusha.look("広場の真ん中にある五メートルほどの建国記念碑の周りを花壇が囲み",
+                "その外側に円形に並ぶようにしてマーケットが広がっていた。",
+                "それが今朝のことだ。",
+                "彼は眉を顰めた母親にわざわざ王直々の呼び出しだと叩き起こされ", "不機嫌さを抱えてここを通ったのだが",
+                "その時には客も集まり", "賑やかな有様だった"),
+            yusha.feel("生温かい風を感じて",
+                "彼は自分の背後を振り返る"),
+            yusha.look("そこには彼が持つのより一回り小さな$phoneを持つ老人が立っていて",
+                "それを彼に向けて小声で何か呟く"),
+            yusha.talk("おい"),
+            yusha.look("一瞬の眩しさを感じて顔の前に手をやったが",
+                "視界が戻るとその老人の姿は既に小さくなってしまっていた"),
+            yusha.talk("何なんだよ……"),
+            yusha.move("$Sは舌打ちをし", "とりあえずと酒場の方に歩き出す。",
+                "流石に魔王退治の旅に一人で出かける訳にもいかない。",
+                "傭兵か", "あるいは仲間でも見繕おうという算段だ"),
+            )
+
+def sc_theend(w: wd.World):
+    yusha, bar = w.yusha, w.barmaster
+    return w.scene("そして終わりは突然に",
+            yusha.go(w.stage.homefield),
+            yusha.talk("何すんだ！"),
+            bar.talk(),# TODO: 酒場から追い出され、別の町で見つけると外、そこで一網打尽
             )
 
 # episodes
@@ -49,6 +88,8 @@ def ep1(w: wd.World):
                 w.yusha.deal(w.phone),
                 w.yusha.go(w.i.trouble),
             sc_Iamyusha(w),
+            sc_strangetown(w),
+            sc_theend(w),
             )
 
 # outline
@@ -67,6 +108,7 @@ def story(w: wd.World):
             )
 
 def main(): # pragma: no cover
+    from src.yunow.story import world
     w = world()
     return w.build(story(w))
 
