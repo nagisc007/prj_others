413186fea3d94790656c5ccc4b50dbd2da398326
nagisc007 <nagisc007@yahoo.co.jp>
2019-07-28 21:48:23 +0900

    Fixed: coverage

--- src/yunow/chapter1b.py
+++ src/yunow/chapter1b.py
@@ -497,9 +497,9 @@ def story_outline(w: wd.World):
 # main
 def story(w: wd.World):
     return (w.maintitle(cnf.TITLE["chap1"]),
-            #ep6(w),
-            #ep7(w),
-            #ep8(w),
+            ep6(w),
+            ep7(w),
+            ep8(w),
             ep9(w),
             )
 
