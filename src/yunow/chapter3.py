41f5b75a6266a5f605748fdc40073deeac112e45
nagisc007 <nagisc007@yahoo.co.jp>
2019-07-30 13:24:56 +0900

    Fixed: episode name

--- src/yunow/chapter3.py
+++ src/yunow/chapter3.py
@@ -102,11 +102,7 @@ def story_outline(w: wd.World):
 # main
 def story(w: wd.World):
     return (w.maintitle(cnf.TITLE["chap1"]),
-            ep6(w),
-            ep7(w),
-            ep8(w),
-            ep9(w),
-            ep10(w),
+            ep11(w),
             )
 
 def main(): # pragma: no cover
