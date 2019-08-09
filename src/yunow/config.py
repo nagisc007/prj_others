10a61589b448b1d67978a349c85af4866369b26f
nagisc007 <nagisc007@yahoo.co.jp>
2019-08-09 17:36:23 +0900

    [WIP] Added: soround goblin

--- src/yunow/config.py
+++ src/yunow/config.py
@@ -51,6 +51,7 @@ STAGES = (
         ("freema", "モウカリ"),
         ("amazon", "mamazon"),
         ("town2", "クレーベ"),
+        ("forest1", "クレーベ周辺の森"),
         # Part
         ("castle1gate", "城門"),
         ("townstreet1", "路地"),
