56c76f4742d5bb048d9f1557cc61ba17756b4780
nagisc007 <nagisc007@yahoo.co.jp>
2019-08-10 21:25:50 +0900

    Fixed: detail and id get

--- src/zebra/config.py
+++ src/zebra/config.py
@@ -9,7 +9,7 @@ sys.path.append('storybuilder')
 
 CHARAS = (
         # main
-        ("emi", "槙坂,笑美", 16, "female", "高校生", "me:わたし:S:わたし:ken:ケンさんi:yumi:遊美:kmam:リアさん:masa:鈴村マサト"),
+        ("emi", "槙坂,笑美", 16, "female", "高校生", "me:わたし:S:わたし:ken:ケンさん:yumi:遊美:kmam:リアさん:masa:鈴村マサト"),
         ("ken", "陣野,ケン", 21, "male", "アパレル経営", "me:オレ:emi:エミちゃん:masa:マサ"),
         # sub
         ("masa", "鈴村,マサト", 27, "male", "カメラマン", "me:俺:ken:ケン"),
