ca639e507455b3eecf511d0d51eea88e28fbfaf8
nagisc007 <nagisc007@yahoo.co.jp>
2019-08-10 16:47:33 +0900

    [WIP] Added: last scene

--- src/zebra/config.py
+++ src/zebra/config.py
@@ -9,10 +9,10 @@ sys.path.append('storybuilder')
 
 CHARAS = (
         # main
-        ("emi", "槙坂,笑美", 16, "female", "高校生", "me:わたし:S:わたし:ken:ケンさんi:yumi:遊美:kmam:リアさん"),
+        ("emi", "槙坂,笑美", 16, "female", "高校生", "me:わたし:S:わたし:ken:ケンさんi:yumi:遊美:kmam:リアさん:masa:鈴村マサト"),
         ("ken", "陣野,ケン", 21, "male", "アパレル経営", "me:オレ:emi:エミちゃん:masa:マサ"),
         # sub
-        ("masa", "鈴村,マサト", 27, "male", "カメラマン", "me:俺"),
+        ("masa", "鈴村,マサト", 27, "male", "カメラマン", "me:俺:ken:ケン"),
         ("yumi", "武田,遊美", 16, "female", "高校生", "me:あたし:emi:エミ"),
         # family
         ("mamken", "陣野,リア", 42, "female", "パート", "me:私i:ken:ケン:emi:笑美ちゃん"),
