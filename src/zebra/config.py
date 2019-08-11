e765419ac2c5f7051c11fe551312292f0fe60ad9
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-08-11 09:23:05 +0900

    Added #104: draft in zebra project (#115)
    * Added: first scene
    
    * [WIP] Added: 2nd scene
    
    * [WIP] Added: in classroom
    
    * Added: 2nd scene
    
    * [WIP] Added: meet zebra
    
    * [WIP] Added: get bra
    
    * [WIP] Added: in his house
    
    * [WIP] Added: zebra bra
    
    * Added: model
    
    * Added: dislike scene
    
    * [WIP] Added: last scene
    
    * [WIP] Added: last part
    
    * Added: last
    
    * Fixed: detail and id get
    
    * Fixed: typo

--- src/zebra/config.py
+++ src/zebra/config.py
@@ -9,13 +9,17 @@ sys.path.append('storybuilder')
 
 CHARAS = (
         # main
-        ("emi", "槙坂,笑美", 15, "female", "高校生", "me:わたし:S:わたし:ken:ケンさん"),
+        ("emi", "槙坂,笑美", 16, "female", "高校生", "me:わたし:S:わたし:ken:ケンさん:yumi:遊美:kmam:リアさん:masa:鈴村マサト"),
         ("ken", "陣野,ケン", 21, "male", "アパレル経営", "me:オレ:emi:エミちゃん:masa:マサ"),
         # sub
-        ("masa", "鈴村,マサト", 27, "male", "カメラマン", "me:俺"),
+        ("masa", "鈴村,マサト", 27, "male", "カメラマン", "me:俺:ken:ケン"),
+        ("yumi", "武田,遊美", 16, "female", "高校生", "me:あたし:emi:エミ"),
         # family
-        ("mamken", "ケンのママ", 42, "female", "パート", "me:私i:ken:ケン:emi:笑美ちゃん"),
+        ("mother", "槙坂,母", 40, "female", "パート", "me:私:emi:笑美"),
+        ("mamken", "陣野,リア", 42, "female", "パート", "me:私i:ken:ケン:emi:笑美ちゃん"),
         # mob
+        ("teacher", "先生", 50, "male", "高校教師", "me:私"),
+        ("murata", "村田,健吾", 67, "male", "農家", "me:僕"),
         )
 
 STAGES = (
@@ -26,6 +30,7 @@ STAGES = (
         ("myhome", "槙坂家"),
         ("kenapart", "ケンのアパート"),
         ("hisshop", "ケンの店"),
+        ("temple", "巌詠寺"),
         # Part
         ("myroom", "笑美の部屋"),
         ("living", "リビング"),
@@ -36,7 +41,8 @@ STAGES = (
 
 DAYS = (
         # main
-        ("current", "現在"),
+        ("marason", "マラソン大会", 10,25, 2019),
+        ("current", "現在", 10,28, 2019),
         ("getzebra", "ゼブラブラ入手日"),
         ("camera", "撮影日"),
         # sub
@@ -51,6 +57,7 @@ ITEMS = (
 INFOS = (
         # main
         ("brandname", "KenKen"),
+        ("Ilove", "アイラブシマウマ"),
         )
 
 
