37ec23e5a76e45811f60aa647d9beab6ab8fd7ef
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-08-18 10:10:13 +0900

    Draft yu ep30 (#131)
    * [WIP] Added: scenes on ep30
    
    * [WIP] Added: first part
    
    * Added: vila front
    
    * Fixed: detail

--- src/yunow/config.py
+++ src/yunow/config.py
@@ -29,7 +29,7 @@ CHARAS = (
         ("muramasa", "ムラサメ", 58, "male", "刀鍛冶", "me:ワシ"),
         ("idol", "ユカリン", 40, "female", "アイドル"),
         ("gatekeeper", "門番", 28, "male", "兵士", "me:私"),
-        ("vilaman1", "村の男性", 45, "male", "村人", "me:私"),
+        ("vilaman1", "村の男性", 45, "male", "村人", "me:私:my:俺"),
         ("uru", "ウル", 68, "female", "宿屋の主人", "me:ワシ"),
         # monster
         ("slime", "スライム", 99, "male", "モンスター", "me:ボク"),
@@ -96,6 +96,7 @@ ITEMS = (
         ("shortsword", "ショートソード"),
         ("karebasho", "今ソコなう"),
         ("gmap", "ゴーゴンマップ"),
+        ("smacache", "スマフ決済"),
         )
 
 
@@ -120,6 +121,7 @@ INFOS = (
         ("mentosgeyser", "メンタスゲイザー"),
         ("selphy", "セルフィン"),
         ("message", "メッセ"),
+        ("makoword001", "何でもはできませんよ。アプリがあるだけ"),
         )
 
 
