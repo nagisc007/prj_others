9f295d590e8bb1de1227ea841b5d064160395d6d
nagisc007 <nagisc007@yahoo.co.jp>
2019-09-01 13:50:45 +0900

    Added: arika project

---
+++ src/arika/config.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""Config: The whereabouts
+"""
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append('storybuilder')
+
+
+CHARAS = (
+        # main
+        # sub
+        # family
+        # mob
+        )
+
+STAGES = (
+        # Area
+        # Place
+        # Part
+        )
+
+DAYS = (
+        # main
+        ("current", "現在", 10,28, 2019),
+        # sub
+        )
+
+ITEMS = (
+        # main
+        # sub
+        )
+
+INFOS = (
+        # main
+        )
+
+
+FLAGS = (
+        )
+
+THEMES = {
+        }
+
+MOTIFS = {
+        }
+
