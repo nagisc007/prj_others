6717d0e56cc45dc39b572e7454963f032306ce76
nagisc007 <nagisc007@yahoo.co.jp>
2019-09-03 10:44:44 +0900

    [WIP] Added: first part scene

--- src/arika/config.py
+++ src/arika/config.py
@@ -9,8 +9,9 @@ sys.path.append('storybuilder')
 
 CHARAS = (
         # main
-        ("mio", "夏草,美緒", 17, "female", "学生", "me:わたし"),
+        ("mio", "夏草,美緒", 17, "female", "学生", "me:わたし:S:わたし"),
         # sub
+        ("haru", "稲葉,春彦", 25, "male", "会社員", "me:俺:mio:美緒ちゃん"),
         # family
         ("mam", "夏草,母", 42, "female", "パート", "me:私"),
         ("dad", "夏草,父", 43, "male", "会社員", "me:僕"),
