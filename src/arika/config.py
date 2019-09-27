d9edf57a78886d1e187f346f2bb10c0373c5ea71
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-09-27 09:05:19 +0900

    Draft ari ep2 (#175)
    * [WIP] Added: first part
    
    * [WIP] Added: talk ochi
    
    * Added: old girl friend
    
    * [WIP] Added: wait him
    
    * [WIP] Added: ride his car
    
    * [WIP] Added: find him different
    
    * Added: meet him
    
    * [WIP] Added: in the karaoke
    
    * [WIP] Added: confess partner
    
    * [WIP] Added: eating
    
    * [WIP] Added: eat finish
    
    * [WIP] Added: in karaoke box
    
    * Added: karaoke scene
    
    * [WIP] Added: night walk
    
    * [WIP] a little desc
    
    * [WIP] Added: back to myroom
    
    * Added: on the bed
    
    * [WIP] Added: catch his msg
    
    * [WIP] Added: goto living in 2day
    
    * [WIP] Added: day2 morning eat
    
    * Added: day2 morning dyning
    
    * [WIP] Added: meet ochi
    
    * [WIP] Added: confess truth ochi
    
    * [WIP] Added: wip
    
    * Added: her status
    
    * [WIP] Added: contact him
    
    * [WIP] Added: goto movie
    
    * [WIP] Added: in the movie theater

--- src/arika/config.py
+++ src/arika/config.py
@@ -9,16 +9,17 @@ sys.path.append('storybuilder')
 
 CHARAS = (
         # main
-        ("mio", "夏草,美緒", 17, "female", "学生", "me:わたし:S:わたし:asano:右輪さん:asachi:麻乃さん:inaba:稲葉さん:haru:春彦さん:yamane:山根さん"),
+        ("mio", "夏草,美緒", 17, "female", "学生",
+            "me:わたし:S:わたし:asano:右輪さん:asachi:麻乃さん:inaba:稲葉さん:haru:春彦さん:yamane:山根さん:ochi:越智さん"),
         # sub
-        ("haru", "稲葉,春彦", 25, "male", "会社員", "me:俺:mio:美緒ちゃん"),
+        ("haru", "稲葉,春彦", 25, "male", "会社員", "me:僕:mio:美緒ちゃん"),
         ("asano", "右輪,麻乃", 17, "female", "学生", "me:うち:mio:美緒っち"),
-        ("ochi", "越智,侑李", 17, "female", "学生", "me:私:mio:美緒"),
+        ("ochi", "越智,侑李", 17, "female", "学生", "me:私:mio:夏草さん"),
         ("yamane", "山根,百合", 17, "female", "学生", "me:私:mio:美緒さん"),
         ("akimoto", "秋本,修二", 17, "male", "学生", "me:僕:mio:夏草さん"),
         # family
-        ("mam", "夏草,母", 42, "female", "パート", "me:私:mio:美緒"),
-        ("dad", "夏草,父", 43, "male", "会社員", "me:僕:mio:美緒"),
+        ("mam", "夏草,明美", 42, "female", "パート", "me:私:mio:美緒"),
+        ("dad", "夏草,芳正", 43, "male", "会社員", "me:僕:mio:美緒"),
         ("bro", "夏草,正則", 15, "male", "中学生", "me:オレ:mio:姉ちゃん"),
         # mob
         )
@@ -35,7 +36,9 @@ STAGES = (
         ("karaoke1", "カラオケ店"),
         ("restaurant1", "レストラン"),
         ("station", "駅前"),
+        ("movie1", "映画館"),
         # Part
+        ("onstreet", "路地"),
         ("myroom", "自室"),
         ("dyning", "食堂"),
         ("living", "リビング"),
